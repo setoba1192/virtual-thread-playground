e224a26d6acebe5040ece930672795d78848bcfd
Joan <setoba1192@hotmail.com>
2025-10-30 21:46:41 -0500

    Thread builder factory method

--- src/main/java/com/setoba/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/com/setoba/sec01/InboundOutboundTaskDemo.java
@@ -1,18 +1,43 @@
 package com.setoba.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
 
-  private static final int MAX_PLATFORM = 1000_000;
+  private static final int MAX_PLATFORM = 10;
 
-  public static void main(String[] args) {
-    platformThreadDemo();
+  public static void main(String[] args) throws InterruptedException {
+    platformThreadDemo3();
   }
 
-  private static void platformThreadDemo() {
+  private static void platformThreadDemo1() {
     for (int i = 0; i < MAX_PLATFORM; i++) {
       int finalI = i;
       Thread thread = new Thread(() -> Task.ioIntensive(finalI));
       thread.start();
     }
+  }
+
+  private static void platformThreadDemo2() {
+    var builder = Thread.ofPlatform().name("seto", 1);
+    for (int i = 0; i < MAX_PLATFORM; i++) {
+      int finalI = i;
+      Thread thread = builder.unstarted(() -> Task.ioIntensive(finalI));
+      thread.start();
+    }
+  }
+
+  private static void platformThreadDemo3() throws InterruptedException {
+    var latch = new CountDownLatch(MAX_PLATFORM);
+    var builder = Thread.ofPlatform().daemon().name("daemon", 1);
+    for (int i = 0; i < MAX_PLATFORM; i++) {
+      int finalI = i;
+      Thread thread = builder.unstarted(() -> {
+        Task.ioIntensive(finalI);
+        latch.countDown();
+      });
+      thread.start();
+    }
+    latch.await();
   }
 }
