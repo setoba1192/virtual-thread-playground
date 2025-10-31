1714931eb88df86f1e1183a5f3c25953bee4f1fd
Joan <setoba1192@hotmail.com>
2025-10-30 22:13:24 -0500

    Virtual Thread Scaling - Demo

--- src/main/java/com/setoba/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/com/setoba/sec01/InboundOutboundTaskDemo.java
@@ -5,11 +5,16 @@ import java.util.concurrent.CountDownLatch;
 public class InboundOutboundTaskDemo {
 
   private static final int MAX_PLATFORM = 10;
+  private static final int MAX_VIRTUAL = 250_000;
+
 
   public static void main(String[] args) throws InterruptedException {
-    platformThreadDemo3();
+    virtualThreadDemo();
   }
 
+  /**
+   * To create a simple java platform thread
+   */
   private static void platformThreadDemo1() {
     for (int i = 0; i < MAX_PLATFORM; i++) {
       int finalI = i;
@@ -18,6 +23,9 @@ public class InboundOutboundTaskDemo {
     }
   }
 
+  /**
+   * To create a platform thread using Thread.Builder
+   */
   private static void platformThreadDemo2() {
     var builder = Thread.ofPlatform().name("seto", 1);
     for (int i = 0; i < MAX_PLATFORM; i++) {
@@ -27,10 +35,32 @@ public class InboundOutboundTaskDemo {
     }
   }
 
+  /**
+   * To create platform daemon thread using Thread.Builder
+   *
+   * @throws InterruptedException
+   */
   private static void platformThreadDemo3() throws InterruptedException {
     var latch = new CountDownLatch(MAX_PLATFORM);
     var builder = Thread.ofPlatform().daemon().name("daemon", 1);
     for (int i = 0; i < MAX_PLATFORM; i++) {
+      int finalI = i;
+      Thread thread = builder.unstarted(() -> {
+        Task.ioIntensive(finalI);
+        latch.countDown();
+      });
+      thread.start();
+    }
+    latch.await();
+  }
+
+  /**
+   * To create a virtual thread using Thread.Builder
+   */
+  private static void virtualThreadDemo() throws InterruptedException {
+    var latch = new CountDownLatch(MAX_VIRTUAL);
+    var builder = Thread.ofVirtual().name("seto-virtual", 1);
+    for (int i = 0; i < MAX_VIRTUAL; i++) {
       int finalI = i;
       Thread thread = builder.unstarted(() -> {
         Task.ioIntensive(finalI);
